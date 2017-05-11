cc78bc4234cc6be12e4d26379133b33b99cf488c
pipi <chenyingfengscdx@163.com>
2017-05-11 10:42:23 +0800

    Victory commit test

--- app/src/main/java/com/example/uibestpractice/MainActivity.java
+++ app/src/main/java/com/example/uibestpractice/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("MainActivity", "onCreate: hh");
         initMsgs(); // 初始化消息数据
         inputText = (EditText) findViewById(R.id.input_text);
         send = (Button) findViewById(R.id.send);
